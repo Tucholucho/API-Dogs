5571c15d5f4f0f51e4a10576c8dc62f67fdbbc35
Tucholucho <lucholp17@gmail.com>
2023-11-03 00:04:56 -0300

    Rutas avanzadas

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -27,6 +27,10 @@ const getDBInfo = async () => {
     return await Dog.findAll({
         include: {
             model: Temperament,
+            attributes: ["name"],
+            through: {
+                attributes: [],
+            }
         }
     })
 }
