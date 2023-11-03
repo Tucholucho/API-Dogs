604dcf55b82052d015e5277fd37aa0dc152205ab
Tucholucho <lucholp17@gmail.com>
2023-11-02 23:16:29 -0300

    Rutas avanzadas

--- api/src/db.js
+++ api/src/db.js
@@ -31,12 +31,12 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { Dog, Temperaments } = sequelize.models;
+const { Dog, Temperament } = sequelize.models;
 
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
-Dog.belongsToMany (Temperaments, {through : "dog_temperamets"});
-Temperaments.belongsToMany (Dog,{through : "dog_temperamets"});
+Dog.belongsToMany (Temperament, {through : "dog_temperamets"});
+Temperaments.belongsToMany (Dog, {through : "dog_temperamets"});
 
 
 module.exports = {
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,4 +1,5 @@
 const { Router } = require('express');
+const axios = require('axios');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
@@ -7,6 +8,28 @@ const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
+const getAPIInfo = async () => {
+    const apiURL = await axios.get("https://api.thedogapi.com/v1/breeds");
+    const apiInfo = await apiURL.data.map(el => {
+        return{
+            name: el.name,
+            image: el.image,
+            height: el.height.map (el => el),
+            weight: el.weight.map (el => el),
+            age: el.life_span,
+            temperament: el.temperament,
+        };
+    });
+    return apiInfo;
+};
+
+const getDBInfo = async () => {
+    return await Dog.findAll({
+        include: {
+            model: Temperament,
+        }
+    })
+}
 
 
 module.exports = router;
